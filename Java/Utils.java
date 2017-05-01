12c47d5e5830885f1a0938a5aef4a1ca0e9c5b0f
Daniel Au <audaniel94@gmail.com>
2017-05-01 02:00:46 -0700

    Java program with static methods.
    Utils contains static methods that are used by multiple programs.  It is
    used to find a file's extension and create a filenmae with formatting
    for photos.

---
+++ Java/Utils.java
@@ -0,0 +1,51 @@
+/** Utility class with static methods. */
+
+import java.util.regex.*;
+
+public class Utils {
+  /**
+   * getExtension() returns the extension of filename that is passed in
+   */
+  public static String getExtension(String fileName) {
+		String extension = "";
+    Pattern extensionPattern = Pattern.compile("\\.\\w+\\z");
+    Matcher extensionMatcher = extensionPattern.matcher(fileName);
+    if (extensionMatcher.find()) {
+			extension = extensionMatcher.group();
+    }
+    return extension;
+  }
+
+  public static String fileName(String prefix, int index, String extension) {
+    StringBuilder fileName = new StringBuilder(prefix);
+
+		fileName.append('_');
+		if (index < 10) {
+			fileName.append("000");
+		} else if (index < 100) {
+			fileName.append("00");
+		} else {
+			fileName.append('0');
+		}
+		fileName.append(index);
+		fileName.append(extension);
+		return fileName.toString();
+  }
+
+  /**
+   * newFileName() takes in the original fileName of the file, the prefix which
+   * the photo should be renamed to, and the photo number.  From there, it
+   * constructs the new filename and keeps the file extension.
+   * @param originalFileName - the original filename of the photo, used to find
+   * the file extension
+   * @param prefix - the prefix to be renamed to
+   * @param index - the number of the photo
+   * @return the new filename Instead of using patterns and matchers, it
+   * would have been easy to look at the last 4 or 5 characters of the filename
+   * and see where the '.' was to get either the 3 or 4 character extension.
+   */
+  public static String newFileName(String originalFileName, String prefix, int index) {
+    String extension = getExtension(originalFileName);
+    return fileName(prefix, index, extension);
+	}
+}
