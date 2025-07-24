0c242f44e5da3b16f75377e952839eae1192ae9d
Daniel <audaniel94@gmail.com>
2025-07-24 14:01:03 -0700

    fix: renames all the photos
    uses the `rand` crate to create a random prefix to prevent overwriting,
    then renames all of those again. Rewrote a lot of the `unwrap`
    statements to use match statements and log better when we panic

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,12 @@
+use rand::distr::{Alphanumeric, SampleString};
 use std::collections::HashSet;
 use std::env;
 use std::fs;
 use std::io::{self, BufRead};
 use std::path::Path;
+
+
+const PREFIX_LENGTH: u32 = 6;
 
 /// Create the HashSet of image extensions
 fn _get_image_extensions() -> HashSet<String> {
@@ -16,57 +20,124 @@ fn _get_image_extensions() -> HashSet<String> {
     image_extensions
 }
 
-/// read a line from stdin
-fn _read_input() -> String {
+
+/// read a line from stdin, strips the leading and trailing whitespace
+fn _read_input() -> Result<String, io::Error> {
     let mut buffer = String::new();
     let stdin = io::stdin();
-    let mut handle = stdin.lock();
-
-    handle.read_line(&mut buffer).unwrap();
-    buffer.trim().to_string()
+    let read_res: io::Result<usize>;
+    {
+        let mut handle = stdin.lock();
+        read_res = handle.read_line(&mut buffer);
+    }
+    match read_res {
+        Ok(_) => Ok(buffer.trim().to_string()),
+        Err(e) => Err(e),
+    }
 }
+
 
 /// prompts the user for a new prefix, and returns it
 ///
 /// if the user enters in `.`, read the directory name as the new prefix
 fn _get_new_prefix() -> String {
     println!("What should the new prefix be?");
-    let mut new_prefix = _read_input();
+    let mut new_prefix = match _read_input() {
+        Ok(prefix) => prefix,
+        Err(e) => panic!("Error: failed to read new prefix: {}", e),
+    };
     if new_prefix == "." {
-        let cwd = env::current_dir().unwrap();
+        let cwd = match env::current_dir() {
+            Ok(cwd) => cwd,
+            Err(e) => panic!("Error: failed to get current directory: {}", e),
+        };
         let full_path = Path::new(&cwd);
-        let file_name = full_path.file_name().unwrap();
-        new_prefix = file_name.to_str().unwrap().to_string();
+        let file_name = match full_path.file_name() {
+            Some(file_name) => file_name,
+            None => panic!("Error: failed to get the top folder from the current directory path."),
+        };
+        new_prefix = match file_name.to_str() {
+            Some(file_name) => file_name.to_string(),
+            None => panic!("Error: failed to convert top folder to a string."),
+        };
     }
     new_prefix
 }
+
+
+/// Returns the file extension
+fn _get_file_extension(filename: &String) -> String {
+    let filename_path = Path::new(filename);
+    let extension: String = match filename_path.extension() {
+        Some(extension) => extension.to_str().unwrap().to_string(),
+        None => String::new(),
+    };
+    extension
+}
+
 
 /// Grabs all the images in the current directory, filters out the ones that aren't images
 ///
 /// Sorts the files alphabetically
 fn get_images(img_extensions: HashSet<String>) -> Vec<String> {
     let mut image_files: Vec<String> = Vec::new();
-    let entries: fs::ReadDir = fs::read_dir(".").unwrap();
+    let entries: fs::ReadDir = match fs::read_dir(".") {
+        Ok(entries) => entries,
+        Err(e) => panic!("Error: failed to read directory: {}", e),
+    };
     for entry_result in entries {
-        let entry = entry_result.unwrap();
-        let entry_md: fs::Metadata = entry.metadata().unwrap();
+        let entry = match entry_result {
+            Ok(entry) => entry,
+            Err(e) => panic!("Error: failed to read directory entry: {}", e),
+        };
+        let entry_md: fs::Metadata = match entry.metadata() {
+            Ok(md) => md,
+            Err(e) => panic!("Error: failed to read directory entry metadata: {}", e),
+        };
         if entry_md.is_dir() {
             continue;
         }
         let filename: String = entry.file_name().into_string().unwrap();
-        let filename_path = Path::new(&filename);
-        let extension: String = filename_path
-            .extension()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .to_string();
+        let extension: String = _get_file_extension(&filename);
         if img_extensions.contains(&extension.to_lowercase()) {
             image_files.push(filename);
         }
     }
     image_files.sort();
     image_files
+}
+
+
+/// Renames a single image to a new prefix and index
+fn rename_image(image_name: String, new_prefix: &str, index: u32) {
+    if index > 9999 {
+        panic!("Error: index out of range - greater than 9999.");
+    }
+    let extension: String = _get_file_extension(&image_name);
+    let new_image_name: String = format!("{}_{:04}.{}", new_prefix, index, extension);
+    println!("Renaming {} to {}", image_name, new_image_name);
+    match fs::rename(image_name, new_image_name) {
+        Ok(_) => (),
+        Err(e) => panic!("Error: failed to rename image: {}", e),
+    };
+}
+
+
+/// Renames all given photos to a new prefix and index
+fn _rename_all_photos(image_files: Vec<String>, new_prefix: &String, starting_index: &String) {
+    let mut index: u32 = starting_index.parse().unwrap();
+    for image in image_files {
+        rename_image(image, &new_prefix, index);
+        index += 1;
+    }
+}
+
+
+/// Generates a random prefix of a given length
+fn _generate_random_prefix(length: u32) -> String {
+    let length_us = usize::try_from(length).unwrap();
+    let rand_prefix: String = Alphanumeric.sample_string(&mut rand::rng(), length_us);
+    rand_prefix
 }
 
 
@@ -78,28 +149,35 @@ fn get_images(img_extensions: HashSet<String>) -> Vec<String> {
 /// original extension.
 fn rename() {
     println!("Let's rename some stuff!");
-    // prompt user for new prefix
     let new_prefix: String = _get_new_prefix();
-    // prompt user for starting index
     println!("What should the photo numbers start from?");
-    let starting_index: String = _read_input();
+    let starting_index: String = match _read_input() {
+        Ok(prefix) => prefix,
+        Err(e) => panic!("Error, failed to read photo start number: {}", e),
+    };
     println!(
         "Photos will be renamed with prefix: `{}` and starting index: `{}`",
         new_prefix, starting_index,
     );
     // grab all photo and video files in the current directory
-    let image_files: Vec<String> = get_images(_get_image_extensions());
+    let mut image_files: Vec<String> = get_images(_get_image_extensions());
     dbg!(&image_files);
 
-    // rename each file to a random prefix
-    // rename each file to its final name
+    // rename each file to a random prefix. This ensures that if we keep the same prefix and just
+    // update the numbers, nothing will be overwritten
+    let rand_prefix: String = _generate_random_prefix(PREFIX_LENGTH);
+    _rename_all_photos(image_files, &rand_prefix, &starting_index);
+    image_files = get_images(_get_image_extensions());
+    _rename_all_photos(image_files, &new_prefix, &starting_index);
 }
+
 
 /// Copies the images in the current directory.
 fn copy() {
     println!("Placeholder - should be copying stuff!");
     // TODO
 }
+
 
 /// read command line args and execute the correct function
 fn main() {
